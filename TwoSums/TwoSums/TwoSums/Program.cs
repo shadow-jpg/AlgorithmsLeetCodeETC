5a9ad379d5c84c03f4843b546cfdf778ba80ace6
shad0w <shad0w0w1111@gmail.com>
2025-04-06 05:06:34 +0400

    TwoSum upd

--- TwoSums/TwoSums/TwoSums/Program.cs
+++ TwoSums/TwoSums/TwoSums/Program.cs
@@ -4,27 +4,25 @@
     {
         int[] data = new int[10] { 1, 2, 1, 5, 7, 8, 10, 12, 10, 2 };
         data = TwoSum(data,14);
-        foreach (int res in data)
-        {
-            Console.Write(res + " ");
-        }
+        Console.Write($"[{data[0]},{data[1]}]"); 
 
     }
     public static int[] TwoSum(int[] nums, int target)
-    {
-        Dictionary<int, int> res = new(nums.Length);
-        for(int i=0;i< nums.Length; i++)
         {
-            if (i > 0)
+            Dictionary<int, int> res = new(nums.Length);
+            for (int i = 0; i < nums.Length; i++)
             {
-                if (target - nums[i] > 0) {
+                if (target - nums[i] > 0)
+                {
                     if (!res.ContainsKey(target - nums[i]))
-                        res.Add(nums[i], i);
+                    {
+                        if (!res.ContainsKey(nums[i]))
+                            res[nums[i]] = i;
+                    }
                     else
                         return [res[target - nums[i]], i];
                 }
             }
+            return new int[] { };
         }
-        return null;
     }
-}
