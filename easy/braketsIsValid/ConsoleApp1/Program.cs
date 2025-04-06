abe7f72d41e71fcb661a7d4f755c594702fa257e
shad0w <shad0w0w1111@gmail.com>
2025-04-06 06:02:04 +0400

    IsValid Brackets

---
+++ easy/braketsIsValid/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string a = "()[]{}";
+        Console.Write(IsValid(a));
+    }
+    public static bool IsValid(string s)
+    {
+        Stack<byte> brackets = new(s.Length / 2 + 1);
+
+        int res = -1;
+        for(int i = 0; i < s.Length; i++)
+        {
+            switch (s[i]){
+                case '(':
+                    brackets.Push(0);
+                    break;
+                case '[':
+                    brackets.Push(1);
+                    break;
+                case '{':
+                    brackets.Push(2);
+                    break;
+                case ')':
+                    if (brackets.Count > 0)
+                        if (brackets.Pop() == 0)
+                            continue;
+                        else
+                            return false;
+                    else 
+                        return false;
+                case ']':
+                    if (brackets.Count > 0)
+                        if (brackets.Pop() == 1)
+                            continue;
+                        else
+                            return false;
+                    else
+                        return false;
+                case '}':
+                    if (brackets.Count > 0)
+                        if (brackets.Pop() == 2)
+                            continue;
+                        else
+                            return false;
+                    else
+                        return false;
+            }
+        }
+        if (brackets.Count > 0)
+            return false;
+        return true;
+    }
+}
