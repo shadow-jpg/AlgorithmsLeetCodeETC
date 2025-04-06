7d4e57ff1d691eeaaba88174c7613b8abe3931f8
shad0w <shad0w0w1111@gmail.com>
2025-04-06 04:15:07 +0400

    2 more from meeting

---
+++ Queue_delete_N_last_elem/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+    }
+    public IEnumerable<T> FilterLast<T>(IEnumerable<T> source, int n)
+    {
+        var buffer = new Queue<T>();
+
+        foreach (var item in source)
+        {
+            buffer.Enqueue(item);
+
+            if (buffer.Count > n)
+            {
+                yield return buffer.Dequeue();
+            }
+        }
+    }
+}
---
+++ distinct_array/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace a
+{
+    public class Program
+    {
+        public static T[] Distinct<T>(T[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+
+            HashSet<T> seen = new HashSet<T>();
+            List<T> result = new List<T>();
+
+            foreach (T item in array)
+            {
+                if (seen.Add(item)) 
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result.ToArray();
+        }
+        public static void Main()
+        {
+            int[] data = new int[10] { 1,2,1,5,7,8,10,12,10,2};
+            data =Distinct(data);
+            foreach(int res in data)
+            {
+                Console.Write(res + " ");
+            }
+
+        }
+
+    }
+}
+    
+    
